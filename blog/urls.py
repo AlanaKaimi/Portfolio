6dbe2e3eb54902f4f6c72a6ab73611b8469c83bd
AlanaKaimi <ajbryce@live.com>
2019-05-13 17:55:06 -0400

    project-detail working.

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
     path('projects/', views.project_list, name="project_list"),
     path('projects/<slug:slug>', views.project_detail, name='project_detail'),
-    path('resume/', views.resume, name='resume'),
+    path('resume/', views.resume, name="resume"),
 
 ]
