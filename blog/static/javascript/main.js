e8dee5bd19a15911f82cc6bde9e3ae52e7b834ee
AlanaKaimi <ajbryce@live.com>
2019-06-02 11:48:52 -0400

    more/less button working

--- blog/static/javascript/main.js
+++ blog/static/javascript/main.js
@@ -1,6 +1,16 @@
-$('.moreless-button').click(function () {
-  $('.moretext').slideToggle()
-  if ($('.moreless-button').text() == '+') {
+// functions that create shortcuts:
+function qS(selector) {
+  return document.querySelector(selector)
+}
+
+function qSA(selector) {
+  return document.querySelectorAll(selector)
+}
+
+// more/less Button toggle
+
+$('.more').click(function () {
+  if ($(this).text() === '+') {
     $(this).text('-')
   } else {
     $(this).text('+')
