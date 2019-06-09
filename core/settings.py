d895f8128aaa6e5e24e717750637e412d0c96164
AlanaKaimi <ajbryce@live.com>
2019-06-09 11:14:48 -0400

    adjusting allowed_hosts

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://aqueous-tundra-53004.herokuapp.com/']
+ALLOWED_HOSTS = ['.aqueous-tundra-53004.herokuapp.com']
 
 
 # Application definition
